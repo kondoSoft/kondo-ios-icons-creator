44349fcae6e3e2436fd79347f8942bb5983484df
Carlos Gonzalez <cyberpolin@gmail.com>
2016-09-28 14:37:08 -0500

    Add sizes

---
+++ sizes.js
@@ -0,0 +1,31 @@
+module exports = {
+  'ios-image':{
+    'folder-name': 'AppIcon.appiconset',
+    'images':{
+      'Icon-App-20x20@1x': 20,
+      'Icon-App-20x20@2x': 40,
+      'Icon-App-20x20@3x': 60,
+      'Icon-App-29x29@1x': 29,
+      'Icon-App-20x20@2x': 58,
+      'Icon-App-20x20@3x': 87,
+      'Icon-App-40x40@1x': 40,
+      'Icon-App-20x20@2x': 80,
+      'Icon-App-20x20@3x': 120,
+      'Icon-App-57x57@1x': 57,
+      'Icon-App-60x60@1x': 60,
+      'Icon-App-60x60@2x': 120,
+      'Icon-App-76x76@1x': 76,
+      'Icon-App-60x60@2x': 152,
+      'Icon-App-60x60@3x': 228,
+      'Icon-App-83.5x83.5@2x': 157,
+    }
+  },
+  'launch-image':{
+    'folder-name': 'AppIcon.appiconset',
+    'images':{
+      'Default': [768,1004],
+      'Default~ipad': [768,1004]
+    },
+  }
+
+};
